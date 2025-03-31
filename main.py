6847f2345b4b44505910cc9081deb3a0b10debba
Vishal Racheti <vishal.racheti6080@gmail.com>
2025-03-31 16:59:28 +0530

    This is the secound commit

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI() # Create the instance of FASt API
+
+## The app instance is the main component of FastApi application .It is used to configure the application 
+class Custom(BaseModel):
+    name: str
+    age: int
+
+
+#/ ping is the path of the end point
+
+# @app.get() is the decorator is used to define an endpoint 
+@app.get('/ping')
+async def root ():
+    return {"message": "Hello World  !!!"}
+
+@app.get('/')
+async def pong() :
+    return {"message": "pong"} 
+
+@app.get("/blog/comments")
+async def read_comments ():
+    return {"message" : "This is the comments section"}
+
+@app.get('/blog/{blog_id}')
+async def read_blog (blog_id : int):
+    return {'blog_id': blog_id}
+
+
+#Qurey Params
+
+@app.post('/blogs/{blog_ids}')
+async def read_qurey (blog_ids : int , request_body: Custom , q : str = None , name: str = " "):
+    print(request_body.age)
+    print (q, name)
+    return {"blog_ids" : blog_ids, "q" : q , "Name" : name}
 
---
+++ todo2.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+import os 
+
+app = FastAPI() # Creating the instance of Fast API
+
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv('FOO'))
+
+todos = []   # Empty list where we can store the todos , in memory db
+
+class Todo(BaseModel):
+    id : int
+    title: str
+    description: Optional[str] = None
+    compeleted : bool = False
+
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo['id'] == todo_id:
+         return todo
+    return {"error: Todo Not found"}
+
+@app.post("/todos")
+def create_todo(todo: Todo):
+   todos.append(todo.dict()) #Append the todo to the list
+   return todos[-1] # Return the last todo
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+   for todo in todos:
+      if todo['id'] == todo_id:
+         return {"message" :"Todo deleted successfully"}
+   return {"error": "Todo Not found"}
+
+
